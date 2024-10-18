25d7fc241d5a795af6e37b9d839fd679cf15cbc6
JStevens <stevens38@marshall.edu>
2024-10-18 19:17:03 -0400

    weatherRoutes.ts update

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,52 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  const city = req.body.city;
+  try {
+    const weatherData = await WeatherService.getWeatherForCity(city);
+    res.json(weatherData);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
   // TODO: save city to search history
+  try {
+    await HistoryService.addCity(city);
+  } catch (error) {
+    console.error(error);
+    console.log('Error saving city to search history');
+  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (req: Request, res: Response) => {
+  try {
+    const cities = await HistoryService.getCities();
+    res.json(cities);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).json({ message: 'City ID is required' });
+    }
+    await HistoryService.removeCity(req.params.id);
+    res.json({ message: 'City removed from search history' });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
 
 export default router;
